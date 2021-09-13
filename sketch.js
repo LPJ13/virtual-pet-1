263fb32acaa7f3a544103f811d1071dc06ceb966
Lekshmi Parvathy J <86661044+LPJ13@users.noreply.github.com>
2021-09-13 18:09:18 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,13 @@
 //Create variables here
-var dog,dogImage;
 
 function preload()
 {
 	//load images here
-  dogImage = loadImage("images/dogImg.png");
-
 }
 
 function setup() {
 	createCanvas(800, 700);
-  dog = createSprite(400,400,15,15);
-  dog.addImage(dogImage);
-  dog.scale=0.3;
+  
 }
 
 
